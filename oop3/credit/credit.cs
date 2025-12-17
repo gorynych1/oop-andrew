30fd92b8e3d108b2cbd47e47bce4c3e171ecdf62
Egor Brusentsov <gorynych@MacBook-Air-Egor.local>
2025-12-17 11:58:28 +0600

    add3

--- oop3/credit/credit.cs
+++ oop3/credit/credit.cs
@@ -3,16 +3,13 @@ namespace BankCredits
     public abstract class Client
     {
         public decimal MonthlyIncome { get; }
-
         protected Client(decimal monthlyIncome)
         {
             MonthlyIncome = monthlyIncome;
         }
-
         public abstract decimal CalculateCreditLimit();
         public abstract decimal CalculateInterestRate();
     }
-
     public class BankEmployee : Client
     {
         public BankEmployee(decimal income) : base(income) { }
@@ -20,27 +17,21 @@ namespace BankCredits
         public override decimal CalculateCreditLimit() => MonthlyIncome * 20;
         public override decimal CalculateInterestRate() => 5;
     }
-
     public class RegularClient : Client
     {
         public RegularClient(decimal income) : base(income) { }
-
         public override decimal CalculateCreditLimit() => MonthlyIncome * 10;
         public override decimal CalculateInterestRate() => 12;
     }
-
     public class BadCreditHistoryClient : Client
     {
         public BadCreditHistoryClient(decimal income) : base(income) { }
-
         public override decimal CalculateCreditLimit() => MonthlyIncome * 3;
         public override decimal CalculateInterestRate() => 25;
     }
-
     public class PrivilegedClient : Client
     {
         public PrivilegedClient(decimal income) : base(income) { }
-
         public override decimal CalculateCreditLimit() => MonthlyIncome * 8;
         public override decimal CalculateInterestRate() => 8;
     }
--- oop3/transport/transport.cs
+++ oop3/transport/transport.cs
@@ -1,26 +1,21 @@
 using System;
-
 namespace PassengerTransport
 {
     public abstract class Transport
     {
         public string RouteName { get; }
-
         protected Transport(string routeName)
         {
             RouteName = routeName;
         }
-
         public abstract decimal CalculateRevenue();
     }
-
     public class Bus : Transport
     {
         public int PassengerCount { get; }
         public int DiscountPassengers { get; }
         public decimal TicketPrice { get; }
         public decimal DiscountCoefficient { get; }
-
         public Bus(string routeName, int passengerCount, int discountPassengers,
                    decimal ticketPrice, decimal discountCoefficient)
             : base(routeName)
@@ -30,7 +25,6 @@ namespace PassengerTransport
             TicketPrice = ticketPrice;
             DiscountCoefficient = discountCoefficient;
         }
-
         public override decimal CalculateRevenue()
         {
             int fullPricePassengers = PassengerCount - DiscountPassengers;
@@ -38,19 +32,16 @@ namespace PassengerTransport
                    DiscountPassengers * TicketPrice * DiscountCoefficient;
         }
     }
-
     public class Taxi : Transport
     {
         public decimal DistanceKm { get; }
         public decimal PricePerKm { get; }
-
         public Taxi(string routeName, decimal distanceKm, decimal pricePerKm)
             : base(routeName)
         {
             DistanceKm = distanceKm;
             PricePerKm = pricePerKm;
         }
-
         public override decimal CalculateRevenue()
         {
             return DistanceKm * PricePerKm;
@@ -61,14 +52,12 @@ namespace PassengerTransport
     {
         public int PassengerCount { get; }
         public decimal TicketPrice { get; }
-
         public Train(string routeName, int passengerCount, decimal ticketPrice)
             : base(routeName)
         {
             PassengerCount = passengerCount;
             TicketPrice = ticketPrice;
         }
-
         public override decimal CalculateRevenue()
         {
             return PassengerCount * TicketPrice;
